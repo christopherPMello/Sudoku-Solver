c73264fce0bf9fd3ef862a20f8887584f0bbe424
Christopher Mello <Christopherpmello@gmail.com>
2020-12-06 10:36:14 -0800

    pre-deploy attempt

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import HomePage from "./js-components/HomePage"
-import reportWebVitals from "./reportWebVitals"
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,5 +10,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 )
-
-reportWebVitals()
